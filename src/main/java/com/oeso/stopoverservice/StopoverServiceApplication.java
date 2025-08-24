7edf858f4de160bce6c118ef1fe232377ba808e2
ur <dorosi150@gmail.com>
2025-08-24 16:47:46 +0900

    Initial commit

---
+++ src/main/java/com/oeso/stopoverservice/StopoverServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oeso.stopoverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopoverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopoverServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oeso/stopoverservice/StopoverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oeso.stopoverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopoverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
