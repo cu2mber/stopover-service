2601fbbf44e1e25df86db549ad7b9d923049cc67
ur <dorosi150@gmail.com>
2025-09-11 19:53:13 +0900

    refactor : 엔티티 컬럼 제약 및 메서드 구조 개선
    - nullable 추가
    - deletedAt 네이밍 변경
    - update 메서드 추가

--- src/main/java/com/cu2mber/stopoverservice/domain/Stopover.java
+++ src/main/java/com/cu2mber/stopoverservice/domain/Stopover.java
@@ -18,42 +18,49 @@ public class Stopover {
     @Column(name = "stopover_no")
     private Long stopoverNo;
 
-    @Column(name = "stopover_name", length = 255)
+    @Column(name = "stopover_name", length = 255, nullable = false)
     @Comment("경유지이름")
     private String stopoverName;
 
-    @Column(name = "stopover_order", columnDefinition = "tinyint")
+    @Column(name = "stopover_order", columnDefinition = "tinyint", nullable = false)
     @Comment("순서")
     private int stopoverOrder;
 
-    @Column(name = "stopover_deletion", columnDefinition = "tinyint")
+    @Column(name = "stopover_deletion", columnDefinition = "tinyint", nullable = true)
     @Comment("삭제여부")
     private boolean stopoverDeletion;
 
-    @Column(name = "deleted_at")
+    @Column(name = "deleted_at", nullable = true)
     @Comment("삭제일시")
-    private LocalDateTime deleted_at;
+    private LocalDateTime deletedAt;
 
-    @Column(name = "local_no", columnDefinition = "tinyint")
+    @Column(name = "local_no", columnDefinition = "tinyint", nullable = false)
     @Comment("지자체번호")
     private int localNo;
 
-    public Stopover(int localNo, String stopoverName, int stopoverOrder, boolean stopoverDeletion, LocalDateTime deleted_at){
+    private Stopover(int localNo, String stopoverName, int stopoverOrder, boolean stopoverDeletion, LocalDateTime deletedAt){
         this.localNo = localNo;
         this.stopoverName = stopoverName;
         this.stopoverOrder = stopoverOrder;
         this.stopoverDeletion = stopoverDeletion;
-        this.deleted_at = deleted_at;
+        this.deletedAt = deletedAt;
     }
 
     public static Stopover ofNewStopover(int localNo, String stopoverName, int stopoverOrder){
         return new Stopover(localNo, stopoverName, stopoverOrder, false, null);
     }
 
-    public void delete(Long stopoverNo){
-        this.stopoverNo = stopoverNo;
+    public void update(String stopoverName) {
+        this.stopoverName = stopoverName;
+    }
+
+    public void updateOrder(int stopoverOrder){
+        this.stopoverOrder = stopoverOrder;
+    }
+
+    public void delete(){
         this.stopoverDeletion = true;
-        this.deleted_at = LocalDateTime.now();
+        this.deletedAt = LocalDateTime.now();
     }
 
 }
